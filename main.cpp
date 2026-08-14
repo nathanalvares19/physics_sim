8005f01c148a3a652dce35d1672dbf4ee0a72a41
nathanalvares19 <nathanalvares2706@gmail.com>
2026-08-15 00:01:47 +0530

    working

--- main.cpp
+++ main.cpp
@@ -1,77 +1,49 @@
 #include <iostream>
+
 #include "raylib.h"
 #include "raymath.h"
 #include "window.h"
+#include "body.h"
 
-Window window(1000, 500, "Nathan", 30);
-
+// rectangle dimensions
 double width = 300;
 double height = 100;
 
-class Body
-{
-public:
-    double x;
-    double y;
-    double vel;
-    Body(double xi, double yi, double v)
-    {
-        x = xi;
-        y = yi;
-        vel = v;
-    }
-
-    void update()
-    {
-        if (y + height > window.Height)
-        {
-            vel = (-1) * vel;
-            y = window.Height - height;
-            return;
-        }
-        vel = vel + 9.81 * 0.5;
-        y = y + (vel) * (0.5) + (0.5) * (9.81) * (0.5) * (0.5);
-    }
-};
-
 int main()
 {
+    // create window
+    Window window(1000, 500, "Nathan", 30);
 
-    // initialize window
-    window.init_window();
+    // create body
+    Body body(window.width / 2, height / 2, 0);
 
-    double time = 0;
-
-    Body body(window.Width / 2 - width / 2, 0, 0);
-
-    double xi = window.Width / 2 - width / 2;
-    double yi = 0;
-
-    // drawing loop
+    // drawing scene
     while (!WindowShouldClose())
     {
-
+        // begin drawing
         BeginDrawing();
-        ClearBackground(LIME);
+        ClearBackground(WHITE);
 
+        // reset logic
         if (IsKeyPressed(KEY_R))
         {
-            DrawRectangle(body.x, 0, width, height, BLACK);
             body.y = 0;
-            body.vel = 0;
+            body.velocity = 0;
+            DrawRectangle(body.x - width / 2, body.y - height / 2, width, height, RED);
         }
         else
         {
-            DrawRectangle(body.x, body.y, width, height, BLACK);
+            // main drawing logic
+            DrawRectangle(body.x - width / 2, body.y - height / 2, width, height, RED);
+            body.move();
         }
 
-        body.update();
-
+        // end drawing
         EndDrawing();
     }
 
     // close window
-    window.close_window();
+    window.close();
 
     return 0;
 }
